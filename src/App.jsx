9fe9a33326c6d6ff061539201e3dc5a273f3bfee
Thomas <thomas.bowman89@gmail.com>
2025-07-07 19:27:38 +0200

    under udvikling home

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import LandingPage from './pages/LandingPage'
 import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
+import UnderUdvikling from './pages/UnderUdvikling'
 
+function AppContent() {
+  const location = useLocation();
 
-function App() {
+  // Hvis vi er på forsiden "/", så vis kun "UnderUdvikling" siden
+  if (location.pathname === "/") {
+    return <UnderUdvikling />;
+  }
+
+  // Ellers vis navbar + sider
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <main className="space-y-32">
         <Routes>
-          
+          <Route path="/home" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           {/* Tilføj flere sider her */}
         </Routes>
       </main>
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
+
+export default App;
